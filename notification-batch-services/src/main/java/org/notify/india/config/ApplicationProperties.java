379f121c853f7fce87a3283756695b37e417ab80
ankit <soodankit1993@gmail.com>
2019-06-06 01:02:15 +0530

    Added class to read the path from properties.

---
+++ notification-batch-services/src/main/java/org/notify/india/config/ApplicationProperties.java
@@ -0,0 +1,24 @@
+package org.notify.india.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix="application",ignoreUnknownFields=false)
+public class ApplicationProperties {
+	private final Batch batch = new Batch();
+	
+	public Batch getBatch() {
+		return batch;
+	}
+	
+	public static class Batch{
+		private String inputPath = "";
+		
+		public String getInputPath() {
+			return this.inputPath;
+		}
+		
+		public void setInputPath(String inputPath) {
+			this.inputPath = inputPath;
+		}
+	}
+}
