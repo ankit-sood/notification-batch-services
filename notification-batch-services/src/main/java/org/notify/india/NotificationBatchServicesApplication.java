52a6726589d8243964e9f6f9cf501bcdf3718f0b
ankit <soodankit1993@gmail.com>
2019-06-06 00:59:57 +0530

    Adding dependencies for the batch service

---
+++ notification-batch-services/src/main/java/org/notify/india/NotificationBatchServicesApplication.java
@@ -0,0 +1,13 @@
+package org.notify.india;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationBatchServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationBatchServicesApplication.class, args);
+	}
+
+}
