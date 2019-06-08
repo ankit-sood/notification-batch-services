5f253c66cdde830731ccab83f42ada13eb69d27c
ankit <soodankit1993@gmail.com>
2019-06-09 01:11:57 +0530

    Adding @Configuration annotation.

--- notification-batch-services/src/main/java/org/notify/india/config/ApplicationProperties.java
+++ notification-batch-services/src/main/java/org/notify/india/config/ApplicationProperties.java
@@ -1,7 +1,9 @@
 package org.notify.india.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix="application",ignoreUnknownFields=false)
 public class ApplicationProperties {
 	private final Batch batch = new Batch();
