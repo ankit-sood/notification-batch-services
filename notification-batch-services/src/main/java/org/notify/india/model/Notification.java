43af06be9c4a5f69e7f3ad13d67851e2b4058137
ankit <soodankit1993@gmail.com>
2019-06-09 02:13:31 +0530

    Adding JPA annotations to the model class.

--- notification-batch-services/src/main/java/org/notify/india/model/Notification.java
+++ notification-batch-services/src/main/java/org/notify/india/model/Notification.java
@@ -2,13 +2,35 @@ package org.notify.india.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="notification_master")
 public class Notification implements Serializable{
 	private static final long serialVersionUID = 5601549843318835751L;
-
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="notificationSequenceGen")
+	@SequenceGenerator(name="notificationSequenceGen")
+	@Column(name="notification_id")
 	private String id;
+	
+	@Column(name="message")
 	private String message;
+	
+	@Column(name="type")
 	private String type;
+	
+	@Column(name="destiantion_address")
 	private String destiantionAddress;
+	
+	@Column(name="processed")
+	private boolean isProcessed;
 	
 	public Notification() {
 		super();
@@ -20,6 +42,15 @@ public class Notification implements Serializable{
 		this.message = message;
 		this.type = type;
 		this.destiantionAddress = destiantionAddress;
+	}
+	
+	public Notification(String id, String message, String type, String destiantionAddress, boolean isProcessed) {
+		super();
+		this.id = id;
+		this.message = message;
+		this.type = type;
+		this.destiantionAddress = destiantionAddress;
+		this.isProcessed = isProcessed;
 	}
 	
 	public String getId() {
@@ -47,10 +78,20 @@ public class Notification implements Serializable{
 		this.destiantionAddress = destiantionAddress;
 	}
 
+	public boolean isProcessed() {
+		return isProcessed;
+	}
+
+	public void setProcessed(boolean isProcessed) {
+		this.isProcessed = isProcessed;
+	}
+
 	@Override
 	public String toString() {
 		return "Notification [id=" + id + ", message=" + message + ", type=" + type + ", destiantionAddress="
-				+ destiantionAddress + "]";
+				+ destiantionAddress + ", isProcessed=" + isProcessed + "]";
 	}
+	
+	
 	
 }
