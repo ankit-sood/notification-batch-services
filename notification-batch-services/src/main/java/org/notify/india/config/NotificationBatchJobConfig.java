86b1ef9bd508cded4ebfaa7a602b558680d7172c
ankit <soodankit1993@gmail.com>
2019-06-08 12:29:59 +0530

    Adding configuration for batch job along with step config and constants

---
+++ notification-batch-services/src/main/java/org/notify/india/config/NotificationBatchJobConfig.java
@@ -0,0 +1,57 @@
+package org.notify.india.config;
+
+import org.notify.india.constants.JobConstants;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationBatchJobConfig {
+	//Configuration related to jobs
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private JobParametersValidator notificationJobParametersValidator;
+	
+	@Bean
+	JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry) {
+		JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+		postProcessor.setJobRegistry(jobRegistry);
+		return postProcessor;
+	}
+	
+	@Bean
+	public Job notificationBatchJob(Step step) throws Exception{
+		return this.jobBuilderFactory.get(JobConstants.JOB_NAME)
+									 .validator(notificationJobParametersValidator)
+									 .start(step)
+									 .build();
+	}
+	
+	//configuration related to step
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step step() throws Exception{
+		return this.stepBuilderFactory.get(JobConstants.STEP_NAME).tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.err.print("Hello All!!!");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+}
---
+++ notification-batch-services/src/main/java/org/notify/india/constants/JobConstants.java
@@ -0,0 +1,7 @@
+package org.notify.india.constants;
+
+public class JobConstants {
+	public static final String JOB_NAME = "notification-batch-job";
+	public static final String JOB_PARAM_FILE_NAME = "notification-job-file-name";
+	public static final String STEP_NAME = "send-notification-step";
+}
